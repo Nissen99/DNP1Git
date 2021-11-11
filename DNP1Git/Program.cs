bc2942d64804d82ee9ef7d739dcde3835626f68b
Mikkel <Mikkelnj411@gmail.com>
2021-11-11 20:57:00 +0100

    SetUp

---
+++ DNP1Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DNP1Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
